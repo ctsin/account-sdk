edcd690169b7a459b7373cbcde888e484c55d8a0
邢贵志 <xinggz@zhongan.com.cn>
2018-09-04 15:01:34 +0800

    更新参数选择器方式

--- src/js/login.ts
+++ src/js/login.ts
@@ -1,7 +1,8 @@
 import { login } from "./sdk/login/init";
+import { $ } from "./sdk/common/utils";
 
 login({
-  container: document.getElementById("login-container"),
+  container: $("#login-container"),
   success() {
     console.log(`调用者传入的成功回调得以执行`);
   }
