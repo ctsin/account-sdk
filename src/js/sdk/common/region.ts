d68257ed8d4e643214265f96464da5cdb4260eb0
邢贵志 <xinggz@zhongan.com.cn>
2018-09-28 18:06:18 +0800

    初步实现下拉菜单

--- src/js/sdk/common/region.ts
+++ src/js/sdk/common/region.ts
@@ -1,25 +1,56 @@
 import { postman } from "./postman";
 import { FormControlRender } from "./interface";
+import { html, render } from "lit-html";
 
 export const selectRender: FormControlRender = async (
   container,
   name = "region"
 ) => {
+  const selectedIndex = {
+    province: 0,
+    city: 0,
+    district: 0
+  };
+
   const provinces = await postman("region").get();
 
-  const tlp = `
-    <div class="region-select-wrapper">
-      <select id="region-area-select">
-        ${provinces
-          .map(
-            province => `
-              <option>${province.name}</option>
-            `
-          )
-          .join("")}
-      </select>
-    </div>
+  const province = () => html`
+    <select on-click="${e => console.log(e)}" id="province-select">
+      ${provinces.map(
+        province => html`
+          <option>${province.name}</option>
+        `
+      )}
+    </select>
   `;
 
-  container.innerHTML = tlp;
+  const city = () => html`
+    <select id="city-select">
+      ${provinces[selectedIndex.province].city.map(
+        city => html`
+          <option>${city.name}</option>
+        `
+      )}
+    </select>
+  `;
+
+  const district = () => html`
+    <select id="district-select">
+      ${provinces[selectedIndex.province].city[
+        selectedIndex.district
+      ].district.map(
+        district => html`
+          <option>${district.name}</option>
+        `
+      )}
+    </select>
+  `;
+
+  const selectTags = () => html`
+    ${province()}
+    ${city()}
+    ${district()}
+  `;
+
+  render(selectTags(), container);
 };
